d002d11a6fa1a1d18ffbda890a1dd655189273a3
Andrea Bruno <andrea_bruno@hotmail.com>
2024-07-28 16:01:01 +0300

    Update

--- ProxyAPISupport/Util.cs
+++ ProxyAPISupport/Util.cs
@@ -63,6 +63,7 @@ namespace ProxyAPISupport
                         if (!CurrentHostFirstRead)
                         {
                             CurrentHostFirstRead = true;
+#if DEBUG                            
                             if (!SpinWait.SpinUntil(() => _CurrentHost != null, 2000))
                             {
                                 // ==================== WARNING: DefaultUrls is not setting at startup !!! ===================
@@ -77,6 +78,7 @@ namespace ProxyAPISupport
                                 // ===========================================================================================
                                 Debugger.Break();
                             }
+#endif
                         }
                     }
                 }
